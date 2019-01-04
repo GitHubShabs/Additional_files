4e98b2433ef9e843786e6eb94454c9599d12af5b
Shabana Mirza <shabanamirza2010@hotmail.com>
2019-01-04 00:45:59 +0000

    test passed to show new instance of bag was created

--- specs/bag_spec.rb
+++ specs/bag_spec.rb
@@ -5,3 +5,15 @@ require_relative('../flight.rb')
 require_relative('../passenger.rb')
 
 class BagTest < MiniTest::Test
+
+  def setup()
+    @bag1 = Bag.new(0, "suitcase")
+  end
+
+  def test_can_create_bag()
+    assert_equal(Bag, @bag1.class())
+  end
+
+
+
+end
